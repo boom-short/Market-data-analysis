ba3228f6d19294c9c1f1b867dbd998b4bb51d63b
boom-short <githubtutorialapp@gmail.com>
2026-01-21 10:29:27 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 # উইনগো ডাটা সোর্স (আপনার API বা URL এখানে দিন)
-API_URL = 'https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage. 
+API_URL = 'https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json';
 
 def fetch_data():
     response = requests.get(URL)
